9df6dfdb74d90adb42e5dde65c297c5055028687
Jessica <je.proenca@yahoo.com.br>
2022-12-08 14:04:06 -0300

    req 1 parte 1 e 2

--- src/App.js
+++ src/App.js
@@ -2,21 +2,49 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.api = this.api.bind(this);
+
+    this.state = {
+      imageUrl: '',
+      isLoading: true,
+    };
+  }
+
   componentDidMount() {
-        
+    this.api();
   }
 
-  shouldComponentUpdate(/* nextProps, nextState */) {
-    // Implemente sua lógica aqui
-    return true;
+  async api() {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const result = await response.json();
+    this.setState({
+      imageUrl: result.message,
+      isLoading: false,
+    });
   }
 
-  componentDidUpdate() {
-    // Implemente sua lógica aqui
-  }
+  // shouldComponentUpdate(/* nextProps, nextState */) {
+  //   // Implemente sua lógica aqui
+  //   return true;
+  // }
+
+  // componentDidUpdate() {
+  //   // Implemente sua lógica aqui
+  // }
 
   render() {
-    return <h1>Doguinhos</h1>;
+    const { isLoading, imageUrl } = this.state;
+    return (
+      <>
+        <h1>Doguinhos</h1>
+        {isLoading
+          ? <p>Loading...</p>
+          : <img src={ imageUrl } alt="Doguinho aleatório" />}
+      </>
+    );
   }
 }
 
